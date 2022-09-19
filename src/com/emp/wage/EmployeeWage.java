4541be84a7a541c3f0320d6d46e2e19210f91611
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2022-09-19 09:10:03 +0530

    Added: Salary using switch case logic

--- src/com/emp/wage/EmployeeWage.java
+++ src/com/emp/wage/EmployeeWage.java
@@ -6,29 +6,28 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		
+
 		Random random = new Random();
-		
+
 		int wagePerHour = 20;
 		int fullDayHours = 8;
 		int halfDayHours = 4;
 		int salary = 0;
-		
+
 		int randomNum = random.nextInt(3);
-		
-		if(randomNum == 1)
-		{
-			System.out.println("Employee  is Present");
-			salary = wagePerHour * fullDayHours;
-		}
-		else if(randomNum == 2)
-		{
-			salary = wagePerHour * halfDayHours;
-		}
-		else
+
+		switch (randomNum) {
+		case 0:
 			System.out.println("Employee  is Absent");
-		
-		System.out.println("Salary is:"+salary);
+			break;
+		case 1:
+			salary = wagePerHour * fullDayHours;
+			break;
+		case 2:
+			salary = wagePerHour * halfDayHours;
+
+		}
+		System.out.println("Salary is:" + salary);
 
 	}
 
