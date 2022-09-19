2c90ef2f3f31e8c579f57e140d8904095a7c9c4c
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2022-09-19 09:19:37 +0530

    Added: Monthly salary logic

--- src/com/emp/wage/EmployeeWage.java
+++ src/com/emp/wage/EmployeeWage.java
@@ -13,21 +13,32 @@ public class EmployeeWage {
 		int fullDayHours = 8;
 		int halfDayHours = 4;
 		int salary = 0;
+		int monthlySalary = 0;
+		int workingDaysPerMonth = 20;
 
-		int randomNum = random.nextInt(3);
+		for (int i = 1; i <= workingDaysPerMonth; i++) {
 
-		switch (randomNum) {
-		case 0:
-			System.out.println("Employee  is Absent");
-			break;
-		case 1:
-			salary = wagePerHour * fullDayHours;
-			break;
-		case 2:
-			salary = wagePerHour * halfDayHours;
+			int randomNum = random.nextInt(3);
+
+			switch (randomNum) {
+			case 0:
+				System.out.println("Employee  is Absent");
+				break;
+			case 1:
+				salary = wagePerHour * fullDayHours;
+				break;
+			case 2:
+				salary = wagePerHour * halfDayHours;
+
+			}
+
+			monthlySalary = monthlySalary + salary;
+
+			System.out.println("Salary is:" + salary);
 
 		}
-		System.out.println("Salary is:" + salary);
+
+		System.out.println("Monthly Salary: " + monthlySalary);
 
 	}
 
